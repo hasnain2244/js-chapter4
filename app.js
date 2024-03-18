dd5a07683e5c7138cea76a4afd041866502fdf3b
hasnain2244 <154471487+hasnain2244@users.noreply.github.com>
2024-03-18 14:23:45 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+//*************************************************//
+//**************** Chapter 4 ***********************//
+//*************************************************//
+
+
+//Question 1. Declare 3 variables in one statement.
+let a,b,c ;
+
+//Question 2. Declare 5 legal & 5 illegal variable names.
+//legal
+let name, $age, _user, total_amount, fullName;
+//illegal
+//let 1num, 0_visit, -User, if, user name;
+
+//Question 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,______ and ______. For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or_____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+document.write("<h1>“Rules for naming JS variables”</h1>")
+
+document.write("<h6>Variable names can only contain 'numbers', '$' and '_'. For example $my_1stVariable</h6>")
+
+document.write("<h6>Variables must begin with a 'letter', '$' or '_'. For example $name, _name or name</h6>")
+
+document.write("<h6>Variable names are case sensitive</h6>")
+
+document.write("<h6>Variable names should not be JS keyords</h6>")
+
+
+//**************************************************************************************************//
+//***********************************The End********************************************************//
+//**************************************************************************************************//
